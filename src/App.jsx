7f337d3251e7952f80c89c853cfcbf00ad55e8b1
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-03-05 12:53:08 -0300

    First commit at project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Arthur Carminatti"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, autem sit."
+      />
+      <Post
+        author="Vanusa Lima"
+        content="Lorem ipsum dolor sit amet com."
+      />
+    </div>
+  )
+}
---
+++ src/Post.jsx
@@ -0,0 +1,10 @@
+export function Post(props) {
+    return(
+        <div>
+            <strong>{props.author}</strong>
+            <p>{props.content}</p>
+            <h1 font-size="16px"> 122323</h1>
+            <hr></hr>
+        </div>
+    )
+}
