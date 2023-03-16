e4ef714ecf541e829c36287a2d9ba2ec9d204d65
arthur-carminatti <arthurcarminattigit@gmail.com>
2023-03-16 14:10:11 -0300

    Testing commit

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,7 +9,7 @@ import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
     const [comments, setComments] = useState([
-        'Post muito bacana, hein?!'
+        'Post muito bacana, amei?!'
     ])
 
     const [newCommentText, setNewCommentText] = useState('')
